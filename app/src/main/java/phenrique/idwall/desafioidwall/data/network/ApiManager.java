9cc078a856f0ce7ae1e34072af224304cef19dc8
Paulo Henrique <phcgaia11@yahoo.com.br>
2019-03-09 14:45:15 -0300

    Added config to retrofit

---
+++ app/src/main/java/phenrique/idwall/desafioidwall/data/network/ApiManager.java
@@ -0,0 +1,34 @@
+package phenrique.idwall.desafioidwall.data.network;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiManager {
+
+    private static ApiInterface REST_CLIENT;
+
+    private static final String API_URL = "https://api-iddog.idwall.co/";
+
+    static {
+        setupRestClient();
+    }
+
+    private ApiManager() {}
+
+    public static ApiInterface get() {
+        return REST_CLIENT;
+    }
+
+    private static void setupRestClient() {
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(API_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient.build())
+                .build();
+
+        REST_CLIENT = retrofit.create(ApiInterface.class);
+    }
+}
