287c4821293c60f9956c409b58cb349c0bbeb803
Paulo Henrique <phcgaia11@yahoo.com.br>
2019-03-09 14:43:53 -0300

    Added class to request singup

---
+++ app/src/main/java/phenrique/idwall/desafioidwall/data/model/RequestLogin.java
@@ -0,0 +1,10 @@
+package phenrique.idwall.desafioidwall.data.model;
+
+public class RequestLogin {
+
+    final String email;
+
+    public RequestLogin(String email) {
+        this.email = email;
+    }
+}
