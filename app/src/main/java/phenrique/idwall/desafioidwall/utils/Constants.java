20f770c91508d6c9c6c0695a140a1e749fa15e14
Paulo Henrique <phcgaia11@yahoo.com.br>
2019-03-11 13:14:12 -0300

    Added classes constants and shared Preference

---
+++ app/src/main/java/phenrique/idwall/desafioidwall/utils/Constants.java
@@ -0,0 +1,17 @@
+package phenrique.idwall.desafioidwall.utils;
+
+public class Constants {
+
+    public static final String ARG_CATEGORY_ID = "arg_category";
+
+    public static final String HUSKY = "husky";
+    public static final String LABRADOR = "labrador";
+    public static final String HOUND = "hound";
+    public static final String PUG = "pug";
+
+    public static final int INT_HUSKY = 0;
+    public static final int INT_LABRADOR = 1;
+    public static final int INT_HOUND = 2;
+    public static final int INT_PUG = 3;
+
+}
